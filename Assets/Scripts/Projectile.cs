2dde2f303382f1641363c5f500fc6a0fff1984f5
LitterBeach <35900848+LitterBeach@users.noreply.github.com>
2019-02-18 20:34:44 -0600

    Attacker Projectile

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+	[SerializeField] private float speed = 1f;
+	
+	private void Update()
+	{
+		transform.Translate(Vector2.right* speed * Time.deltaTime);
+	}
+}
