eec9b2f882298c6081a76994b53540a99d7fa058
LitterBeach <35900848+LitterBeach@users.noreply.github.com>
2019-02-18 19:59:25 -0600

    Animation event, SetMovementSpeed

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -3,16 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Attacker : MonoBehaviour
-{	
-	[Range(0f, 5f)]
-	[SerializeField] private float walkSpeed = 1f;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+{
+	[Range(0f, 5f)] private float _currentSpeed = 1f;	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate(Vector2.left * Time.deltaTime * walkSpeed);
+		transform.Translate(Vector2.left * Time.deltaTime * _currentSpeed);
+	}
+
+	public void SetMovementSpeed(float speed)
+	{
+		_currentSpeed = speed;
 	}
 }
