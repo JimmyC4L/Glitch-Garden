544c181bcf19cb31c2ebfd24c626023552ec94bd
LitterBeach <35900848+LitterBeach@users.noreply.github.com>
2019-02-18 21:09:21 -0600

    Dmg, Health, deathVFX

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,10 +6,23 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
 
-	[SerializeField] private float speed = 1f;
+	[SerializeField] private float speed = 2f;
+	[SerializeField] private float damage = 50f;
 	
 	private void Update()
 	{
 		transform.Translate(Vector2.right* speed * Time.deltaTime);
+	}
+
+	private void OnTriggerEnter2D(Collider2D otherCollider2D)
+	{
+		var health = otherCollider2D.GetComponent<Health>();
+		var attacker = otherCollider2D.GetComponent<Attacker>();
+
+		if (attacker && health)
+		{
+			health.DealDamage(damage);
+			Destroy(gameObject);
+		}
 	}
 }
